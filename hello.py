e858dc49b44cb17917d6acc6900e87dce64e9e83
write4fariha <fariha.khan27@gmail.com>
2023-12-10 12:37:57 +0530

    Adding code for txt file

---
+++ hello.py
@@ -0,0 +1,8 @@
+ip=input("Enter your ip:")
+print("Given IP:" + ip)
+if ip.startswith("10."):
+    print("Class A private IP")
+elif True:
+    print("class C Private IP")
+else:
+    print("NOt class A private IP")
