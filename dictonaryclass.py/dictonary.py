3fee91e85e4b4daac632eaa2a02f724b3f17da47
write4fariha <fariha.khan27@gmail.com>
2024-02-04 12:43:59 +0530

    dictonary practice

--- dictonaryclass.py/dictonary.py
+++ dictonaryclass.py/dictonary.py
@@ -1,10 +1,21 @@
-deviceinfo={
-    "name":"CISCO-Device",
-    "Device":"Router",
-    "numbers":4,
-    "Users_info":[{"name":"fariha","profile":"SSRE"},{"name":"khan","profile":"network eng"}]
-}
-#print(deviceinfo["Users_info"][0]["profile"])
-print(list(deviceinfo["Users_info"][0].values())[1])
+# deviceinfo={
+#     "name":"CISCO-Device",
+#     "Device":"Router",
+#     "numbers":4,
+#     "Users_info":[{"name":"fariha","profile":"SSRE"},{"name":"khan","profile":"network eng"}]
+# }
+# #print(deviceinfo["Users_info"][0]["profile"])
+# print(list(deviceinfo["Users_info"][0].values())[1])
+
+Device_Details = [{"switch":"Meraki"},{"router":"cisco"}]
+
+# for item in Device_Details:
+#     for k,v in item.items():
+#         print((k,v))
+
+t1=[(k,v) for item in Device_Details for k,v in item.items()]
+print(t1)
+
+
 
 
