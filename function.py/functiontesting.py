ed97701c0e2374b1825515740587998ca170f0a8
write4fariha <fariha.khan27@gmail.com>
2024-02-11 14:15:41 +0530

    function multiplication & divsion

--- function.py/functiontesting.py
+++ function.py/functiontesting.py
@@ -1,6 +1,20 @@
-def sum(a,b):
-    s=a+b
-    print(s)
+# def sum(a,b):
+#     s=a+b
+#     print(s)
+#     return s
+# output=sum(5,5)
+# print(output)
+
+
+# def multiplication(x,y):
+#     a=x*y
+#     print(a)
+#     return a
+# output=multiplication(5,5)
+# print(output)
+
+def division(L,M):
+    s=L/M
     return s
-output=sum(5,5)
+output=division(25,5)
 print(output)
