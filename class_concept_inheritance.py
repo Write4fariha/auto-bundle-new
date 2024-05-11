7d6286ed7d541a72e44a49ca1e41a5a1a845f747
write4fariha <fariha.khan27@gmail.com>
2024-05-12 01:56:55 +0530

    class concept with init

---
+++ class_concept_inheritance.py
@@ -0,0 +1,40 @@
+# Learning python class with inheritance
+
+class calc:
+    
+    x=50
+    
+    def __init__(self, y, z):
+        self.y = y
+        self.z = z
+        print("I am in init function")
+        print("Value of Y", y)
+
+    def add(self):
+        print("I am in add Function")
+        print("Value of Y in add function",self.y)
+        print("value of Z in add function",self.z)
+        print(self.y+self.z)
+    
+    def mul(self):
+        print("I am in MUL Function")
+        print("Value of Y in mul function",self.y)
+    
+    def div(self):
+        print("I am in DIV Function")
+        print("Value of Y in div function",self.y)
+    
+       
+def main():
+    obj1 = calc(20,100)
+    obj1.add()
+    obj1.mul()
+    obj1.div()
+    print(obj1.x)
+
+if __name__=="__main__":
+    main()
+
+
+
+
