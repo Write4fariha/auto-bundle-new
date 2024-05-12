ad22a84e7bf16721381bab4a7dc87844b97c50aa
write4fariha <fariha.khan27@gmail.com>
2024-05-12 17:03:45 +0530

    class concept with local arg a & b

--- class_concept_staticmethod.py
+++ class_concept_staticmethod.py
@@ -8,11 +8,13 @@ class calc:
             print("I am in init function")
             print("value of Y in init fucntion",y)
     
-    def add(self):
+#defining arg local to add class function
+ 
+    def add(self,a,b):
             print("I am in Add Function")
-            print("Value of Y in add function",self.y)
-            print("value of Z in add function",self.z)
-            print(self.y+self.z)
+            print("Value of A in add function",a)
+            print("value of B in add function",b)
+            print(a+b)
     
     def mul(self):
         print("I am in MUL Function")
@@ -21,7 +23,7 @@ class calc:
 def main():
       
       obj1=calc(100,50)
-      obj1.add()
+      obj1.add(20,30)
       obj1.mul()
 
 if__name__="__main__"
