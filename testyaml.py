56d4079291ce7314941c3ff3b4b671ee62282587
write4fariha <fariha.khan27@gmail.com>
2024-05-13 02:07:41 +0530

    converting Python data into Yaml data

--- testyaml.py
+++ testyaml.py
@@ -3,17 +3,21 @@ import yaml
 
 # CONVERT YAML DATA TO PYTHON DATA
 
-yamlfile=open("config_data.yaml")
+# yamlfile=open("config_data.yaml")
 
-sw_config_data=yaml.safe_load(yamlfile)
-# print(sw_config_data)
+# sw_config_data=yaml.safe_load(yamlfile)
+# # print(sw_config_data)
 
-# sw_config_data = {
-#     "intf":"gig0/1",
-#     "desc":"connected to server",
-#     "intf_mode":"access",
-#     "vlan_id":"10"
-# }
+sw_config_data = {
+    "intf":"gig0/1",
+    "desc":"connected to server",
+    "intf_mode":"access",
+    "vlan_id":"10"
+}
+
+# CONVERT PYTHON DATA TO YAML DATA
+yamlfile=open("yamlconfig_data.yaml",'w')
+output=yaml.dump(sw_config_data,yamlfile)
 
 sw_config_syntax = {
     "intf":"interface {}",
@@ -22,7 +26,5 @@ sw_config_syntax = {
     "vlan_id":"switchport mode access {}"
 }
 
-
-# print(sw_config_syntax ["intf"].format(sw_config_data["intf"]))
 for k,v in sw_config_data.items():
     print(sw_config_syntax [k].format(v))
