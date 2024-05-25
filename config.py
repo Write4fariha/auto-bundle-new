29888e2fe2502424f75407c4a147c022dc457ec8
write4fariha <fariha.khan27@gmail.com>
2024-05-25 15:44:51 +0530

    writting and reading new file in jinja2 test4

--- config.py
+++ config.py
@@ -22,4 +22,15 @@ template = env.get_template("config_jinja.j2")
 rendered_output = template.render(config_vlan=data) 
 
 # Pirnt rendered output
-print(rendered_output)
+# print(rendered_output)
+
+# opening new file and writting rendered output value in it. It will create new file and dislay the output on it.
+config_file=open("config_gen.data","w")
+config_file.write(rendered_output)
+config_file.close()
+
+
+# it will open existing file created above and read all lines from that file and print the output 
+config_file=open("config_gen.data","r")
+config_list=config_file.readlines()
+print(config_list)
