590000d24f37f7a9533fa62a4219cb4ad27e9703
write4fariha <fariha.khan27@gmail.com>
2024-06-02 02:19:07 +0530

    configuring interface description

--- config_sw_intf.py
+++ config_sw_intf.py
@@ -8,8 +8,9 @@ from netmiko import ConnectHandler
 from jinja2 import FileSystemLoader,Environment
 
 # load yaml file into data and convert it into python format
-file=open("config_data_vlan.yml","r")
-data=yaml.safe_load(file)
+
+# file=open("config_data_sw_intf.yml","r")
+# data=yaml.safe_load(file)
 # print(data)
 
 file=open("inv.yml","r")
@@ -20,11 +21,11 @@ devices=yaml.safe_load(file)
 
 template_loader = FileSystemLoader(searchpath=".")
 env = Environment(loader=template_loader)
-template = env.get_template("config_jinja_vlan.j2")
+template = env.get_template("config_jinja_sw_intf.j2")
 
 # Render the template with the data
-
-rendered_output = template.render(config_vlan=data) 
+data = ""
+rendered_output = template.render(config_data=data) 
 # print(rendered_output) # Print rendered output
 config_list=rendered_output.splitlines()
 
@@ -48,16 +49,14 @@ user_password = os.getenv('PASSWORD')
 
 for device in devices:
     print("Connected to the device",device["hostname"])
-    # print(device["hostname"])
     net_connect = ConnectHandler(device_type=device['device_type'],host=device['host'],username= user,password= user_password)
     
-
 # # Example command: display the vlan & Netmiko different module uses
 
     output = net_connect.send_command("sh vlan bri") # this command use to display the mention command output
     output = net_connect.send_config_set(config_list) # config_set make config changes on switches with data mentioned in config_list file location
     # output = net_connect.send_config_from_file("config_gen_vlan.data") #config_from_file make config changes on switches with  data by passing direct file name itself
-    print("Configuration PUSHED with keeping inventory data outside the code")
+    print("Configuration PUSHED")
     print(output)
 
 # Disconnect from the device
